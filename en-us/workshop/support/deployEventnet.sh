f672e73df3e6333415eda6732639b694b784380e
lllwvlvwlll <lllwvlvwlll@gmail.com>
2019-02-11 17:28:32 -0700

    ...

--- en-us/workshop/support/deployEventnet.sh
+++ en-us/workshop/support/deployEventnet.sh
@@ -1,6 +1,6 @@
 apt-get update
 
-sudo apt-get install \
+sudo -y apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -18,7 +18,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
