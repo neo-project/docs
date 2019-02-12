2b266faec9be29b67cb4df99b673075a3774e19e
lllwvlvwlll <lllwvlvwlll@gmail.com>
2019-02-11 17:14:40 -0700

    ...

--- en-us/workshop/support/deployEventnet.sh
+++ en-us/workshop/support/deployEventnet.sh
@@ -1,12 +1,13 @@
 apt-get update
 
-apt-get -y install \
+sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
     software-properties-common \
     make
+
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -17,7 +18,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
@@ -25,3 +26,4 @@ sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/CityOfZion/neo-local.git
 
 cd ./neo-local && make start
+
